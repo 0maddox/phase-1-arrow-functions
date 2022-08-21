22a86940ce34478c87e6c5919d68a10b634612d2
nicholas <nickkiim7@gmail.com>
2022-08-21 10:21:38 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+let x = 2
+let a = 3
+let b = 4 
+function divide(){
+    return 2000/100
+}
+const square = number =>number **2
+ square(x)
+
+const add  =(number1, number2) =>{
+    return (number1+number2)
+}
+add(a,b)
+
+
+
